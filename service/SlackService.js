10a461667051e04eed481e3b2302763da6497e22
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-05-12 01:12:11 +0900

    feat: クラス設計変更
    - Slack投稿のURL取得処理を別クラスに分割

---
+++ service/SlackService.js
@@ -0,0 +1,23 @@
+// モジュール読み込み
+const networkUtils = require('../utility/networkUtils');
+const { SlackURLConstants } = require('../constants/SlackURLConstants');
+
+class SlackService{
+    constructor(){
+
+    };
+
+    // Slack投稿のURL取得
+    async getPermalink (diaryModel) {
+        var eventTs = diaryModel.eventTs;
+        var channel = diaryModel.channel;
+        var headers = {
+            'Authorization': 'Bearer ' + process.env.SLACK_BOT_USER_ACCESS_TOKEN
+        }
+        const url = `${SlackURLConstants.getPermalink}?channel=${channel}&message_ts=${eventTs}`;
+        var response = await networkUtils.sendHttpRequest(url, 'GET', headers, undefined);
+        return JSON.parse(response).permalink;
+    }
+}
+
+exports.SlackService = SlackService;
