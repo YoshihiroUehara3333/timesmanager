5c7604de0bc4953430a79b1440920b8b9367187f
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-05-12 14:47:33 +0900

    fix: カス

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.command(/.*/, async ({ ack, command, context, logger, client }) => {
     await appCommandController.handleAppCommand(command, context, logger, client);
 });
 
-app.messaage(async ({ ack, message, context, logger, client }) => {
+app.message(async ({ ack, message, context, logger, client }) => {
     await ack();
     logger.info('context出力' + JSON.stringify(context));
     await appMessageController.handleAppCommand(command, context, logger, client);
