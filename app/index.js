5103bb50aeaf6c224a25b8d2d093cfef2b2dc42d
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-05-27 07:13:06 +0900

    feature: モーダル出るようにした

--- app/index.js
+++ app/index.js
@@ -25,18 +25,6 @@ const appCommandController = new AppCommandController(slackPresenter);
 const appMessageController = new AppMessageController(diaryService, twitterService, slackPresenter);
 const appViewController = new AppViewController();
 
-const parseBody = async (req) => {
-    const chunks = [];
-    for await (const chunk of req) {
-        chunks.push(chunk);
-    }
-    const bodyString = Buffer.concat(chunks).toString();
-    try {
-        return JSON.parse(bodyString);
-    } catch {
-        return null;
-    }
-};
 
 // アプリ初期化
 const awsLambdaReceiver = new AwsLambdaReceiver({
@@ -46,22 +34,6 @@ const awsLambdaReceiver = new AwsLambdaReceiver({
 const app = new App({
     token: process.env.SLACK_BOT_USER_ACCESS_TOKEN,
     receiver: awsLambdaReceiver,
-    customRoutes: [
-        {
-            path: '/slack/events', // Slackが送ってくるURLと一致させる
-            method: ['POST'],
-            handler: async (req, res) => {
-                const body = await parseBody(req);
-                if (body && body.challenge) {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(body.challenge);
-                } else {
-                res.writeHead(404);
-                res.end();
-                }
-            }
-        }
-    ]
 });
 
 const handler = awsLambdaReceiver.toHandler();
@@ -90,27 +62,11 @@ app.message(async ({ message, context, logger, client }) => {
 
 app.view('makethread_modal', async ({ body, view, client }) => {
     console.log(`
-        app.view \n
-        body: ${JSON.stringify(body)} \n
-        view: ${JSON.stringify(view)} \n
+    app.view \n
+    body: ${JSON.stringify(body)} \n
+    view: ${JSON.stringify(view)} \n
     `);
-
-    const userId = body.user.id;
-    const metadata = JSON.parse(view.private_metadata);
-    const { channel_id, thread_ts, date } = metadata;
-
-    const title = view.state.values.title_block.title_input.value;
-    const content = view.state.values.content_block.content_input.value;
-
-    // スレッドへの返信
-    await client.chat.postMessage({
-        channel: channel_id,
-        thread_ts: thread_ts,
-        text: `📝 <@${userId}> さんの作業予定\n*タイトル:* ${title}\n*内容:* ${content}`
-    });
-
-    // 必要であればDBに保存（例: DynamoDB）
-    // await dynamo.put({ ... });
+    await appViewController.handleMakeThreadModal(body, view, client);
 });
 
 // ハンドラー生成
