c7527f9a5c312e64eb876a3c011b7139239484af
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-05-27 12:10:56 +0900

    feat: aws-sdkバージョンアップ

--- app/repository/DynamoThreadRepository.js
+++ app/repository/DynamoThreadRepository.js
@@ -1,27 +1,30 @@
 // スレッド情報をDynamo DBに永続化するクラス
 
 // モジュール読み込み
-const AWS = require('aws-sdk');
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, GetCommand, PutCommand, DeleteCommand } = require('@aws-sdk/lib-dynamodb');
+const { DBConst } = require('../constants/DBConst');
 
 class DynamoThreadRepository {
   constructor() {
-    this.dynamodb = new AWS.DynamoDB.DocumentClient();
+    const client = new DynamoDBClient({});
+    this.dynamodb = DynamoDBDocumentClient.from(client);
   };
 
   // データの登録
   async putItem(params) {
-    await this.dynamoDB.putItem(params).promise();
+    return await this.dynamoDB.send(new PutCommand(params));
   }
 
   // データの取得
   async getItem(params) {
-    const result = await this.dynamoDB.getItem(params).promise();
+    const result = await this.dynamoDB.send(new GetCommand(params));
     return result.Item;
   }
 
   // データの削除
   async deleteItem(params) {
-    await this.dynamoDB.deleteItem(params).promise();
+    return await this.dynamoDB.send(new DeleteCommand(params));
   }
 }
 
