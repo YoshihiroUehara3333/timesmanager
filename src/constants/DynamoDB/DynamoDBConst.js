e79a0d5b5296a4be8dd420d8932248c7fc8ec58e
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-06-09 23:59:05 +0900

    #70 mainブランチのファイル削除

--- src/constants/DynamoDB/DynamoDBConst.js
+++
@@ -1,6 +0,0 @@
-// DynamoDB関連の定数定義
-const { POSTDATA }    = require('./PostData');
-const { CHANNELDATA } = require('./ChannelData');
-
-exports.POSTDATA    = POSTDATA;
-exports.CHANNELDATA = CHANNELDATA;
