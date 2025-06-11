731a7d23c69205c705a14b6a5cb15485b747dc43
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-06-11 12:12:11 +0900

    feat: Slack API関連クラスを整備 #73

--- src/adaptor/slack/SlackApiAdaptor.js
+++ src/adaptor/slack/SlackApiAdaptor.js
@@ -1,11 +1,4 @@
-// ControllerからSlackへのメッセージ送信を行うクラス
-//
-//
-// chat.postMessage
-// https://api.slack.com/methods/chat.postMessage
-// chat.getpermalink
-// 
-// views.open
+// Slack Clientのラッパー
 //
 
 class SlackApiAdaptor {
@@ -14,61 +7,33 @@ class SlackApiAdaptor {
     }
 
     // DMを送信する
-    async sendDirectMessage (msg, userId) {
-        return await this.client.chat.postMessage({
-            channel   : userId,
-            text      : msg,
-            mrkdwn    : true
-        });
-    }
-
-    // 対象チャンネルにポストする
-    async sendMessage (msg, channel) {
-        return await this.client.chat.postMessage({
-            channel   : channel,
-            text      : msg,
-            mrkdwn    : true
-        });
-    }
-
-    // 対象チャンネルにポストする
-    async sendBlockMessage (text, channelId, threadTs, blocks) {
-        return await this.client.chat.postMessage({
-            channel   : channelId,
-            thread_ts : threadTs,
-            text      : text,
-            mrkdwn    : true,
-            blocks    : blocks
-        });
-    }
-
-    // スレッド内に返信する
-    async sendThreadMessage (msg, channel, threadTs) {
-        return await this.client.chat.postMessage({
-            channel   : channel,
-            text      : msg,
-            thread_ts : threadTs,
-            mrkdwn    : true
-        });
-    }
-
-    // モーダルを開く
-    async openView (view, triggerId) {
-        return await this.client.views.open({
-                trigger_id : triggerId,
-                view       : view,
-        })
+    async postMessage (request) {
+        try {
+            return await this.client.chat.postMessage(request.toPayload());
+        } catch (error) {
+            throw new Error (error.message, {cause: error});
+        }
     }
 
     // Slack投稿のURLを取得する
-    async getPermalink(channelId, messageTs) {
-        const getResult = await this.client.chat.getPermalink({
-            channel    : channelId,
-            message_ts : messageTs
-        })
+    async getPermalink(request) {
+        try {
+            const getResult = await this.client.chat.getPermalink(request.toPayload());
+            return getResult.permalink;
+        } catch (error) {
+            throw new Error (error.message, {cause: error});
+        }
+    }
 
-        return getResult.permalink;
+    // モーダルを開く
+    async viewsOpen (request) {
+        try {
+            return await this.client.views.open(request.toPayload());
+        } catch (error) {
+            throw new Error (error.message, {cause: error});
+        }
     }
 }
 
 exports.SlackApiAdaptor = SlackApiAdaptor;
+
--- src/adaptor/slack/SlackApiDto.js
+++ src/adaptor/slack/SlackApiDto.js
@@ -1,5 +1,105 @@
-class SlackApiDto {
+// SlackAPIへ送るRequestDTO
 
+// chat.postMessage
+// https://api.slack.com/methods/chat.postMessage
+class SlackPostMessageRequest {
+    constructor(
+        channelId, 
+        text,
+        threadTs,
+        blocks,
+    ){
+        this._channelId = channelId || null;
+        this._text      = text || null;
+        this._threadTs  = threadTs || null;
+        this._blocks    = blocks || null;
+        this._mrkdwn    = true;
+
+        this.validate();
+    }
+
+    validate () {
+        if (!this._channelId || typeof this._channelId !== 'string') {
+            throw new Error("SlackPostMessageRequest: channelId is required and must be a string.");
+        }
+        if (!this._text || typeof this._text !== 'string') {
+            throw new Error("SlackPostMessageRequest: text is required and must be a string.");
+        }
+    }
+
+    toPayload () {
+        let payload = {};
+        payload.channel    = this._channelId;
+        payload.text       = this._text;
+        payload.thread_ts  = this._threadTs ?? undefined;
+        payload.blocks     = this._blocks  ?? undefined;
+        payload.mrkdwn     = this._mrkdwn ?? true;
+        return payload;
+    }
 }
 
-exports.SlackApiDto = SlackApiDto;
+// chat.getpermalink
+// https://api.slack.com/methods/chat.getpermalink
+class SlackGetPermaLinkRequest {
+    constructor (
+        channelId,
+        messageTs,
+    ) {
+        this._channelId = channelId || null;
+        this._messageTs = messageTs || null;
+
+        this.validate();
+    }
+
+    validate () {
+        if (!this._channelId || typeof this._channelId !== 'string') {
+            throw new Error("SlackGetPermaLinkRequest: channelId is required and must be a string.");
+        }
+        if (!this._messageTs || typeof this._messageTs !== 'string') {
+            throw new Error("SlackGetPermaLinkRequest: messageTs is required and must be a string.");
+        }
+    }
+
+    toPayload() {
+        let payload = {}
+        payload.channel = this._channelId;
+        payload.message_ts = this._messageTs;
+        return payload;
+    }
+}
+
+// views.open
+// https://api.slack.com/methods/views.open
+class SlackViewsOpenRequest {
+    constructor (
+        triggerId,
+        view,
+    ) {
+        this._triggerId = triggerId;
+        this._view      = view;
+
+        this.validate();
+    }
+
+    validate() {
+        if (!this._triggerId || typeof this._triggerId !== 'string') {
+            throw new Error("SlackViewsOpenRequest: channelId is required and must be a string.");
+        }
+        if (!this._view || typeof this._view !== 'object') {
+            throw new Error("SlackViewsOpenRequest: messageTs is required and must be a object.");
+        }
+    }
+
+    toPayload () {
+        let payload = {};
+        payload.trigger_id = this._triggerId;
+        payload.view       = this._view;
+        return payload;
+    }
+}
+
+exports.SlackApiDto = {
+    SlackPostMessageRequest  : SlackPostMessageRequest,
+    SlackViewsOpenRequest    : SlackViewsOpenRequest,
+    SlackGetPermaLinkRequest : SlackGetPermaLinkRequest,
+};
