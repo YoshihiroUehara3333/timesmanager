112f47ba779f31a19813856cf736965efd25a810
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-07-31 00:09:51 +0900

    add: SpringBootクラス作成

---
+++ springboot/timesmanager-backend/src/main/java/com/slack_timesmanager/diary/DiaryController.java
@@ -0,0 +1,10 @@
+package com.slack_timesmanager.diary;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/diary")
+public class DiaryController {
+
+}
