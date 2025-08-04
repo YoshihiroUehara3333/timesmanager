b5e70e4af3f076a482ec6e1cdbfd4df7813acb17
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-08-05 03:55:10 +0900

    feat: モーダル実装

--- slackbot/src/blockkit/DailyReportInputModal.js
+++ slackbot/src/blockkit/DailyReportInputModal.js
@@ -35,6 +35,7 @@ exports.DailyReportInputModal = () => ({
 			},
 			element : {
 				type      : "plain_text_input",
+				multiline : true,
 				action_id : "input"
 			}
 		},
@@ -47,6 +48,7 @@ exports.DailyReportInputModal = () => ({
 			},
 			element : {
 				type      : "plain_text_input",
+				multiline : true,
 				action_id : "input"
 			}
 		},
