2c49b46fe1465b92715218be2bba9c8ca5b9ec33
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-08-04 22:48:08 +0900

    feat: CORS設定

--- springboot/timesmanager-backend/src/main/java/com/slack_timesmanager/config/GlobalCorsConfig.java
+++ springboot/timesmanager-backend/src/main/java/com/slack_timesmanager/config/GlobalCorsConfig.java
@@ -1,3 +1,5 @@
+package com.slack_timesmanager.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
