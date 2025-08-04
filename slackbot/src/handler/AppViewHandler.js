2c1ca241cc82b46ee2f06fe8562d9c50015c42d3
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-08-04 17:03:22 +0900

    feat: モーダル実装

--- slackbot/src/handler/AppViewHandler.js
+++ slackbot/src/handler/AppViewHandler.js
@@ -72,7 +72,7 @@ class AppViewHandler extends HandlerBase {
         console.log(JSON.stringify(data));
 
         // バックエンドAPIにPOST送信
-        await axios.post(process.env.BACKEND_API_BASE_URL + '/api/diary', transformedData);
+        await axios.post(process.env.BACKEND_API_BASE_URL + '/api/diary', data);
         return null;
     }
 }
