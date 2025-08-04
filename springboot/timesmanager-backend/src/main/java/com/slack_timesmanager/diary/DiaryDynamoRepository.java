9bb7aa8ccd6a2c7254617fcf0f966af3b20c2ed4
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2025-08-04 22:42:38 +0900

    feat: backend実装

--- springboot/timesmanager-backend/src/main/java/com/slack_timesmanager/diary/DiaryDynamoRepository.java
+++ springboot/timesmanager-backend/src/main/java/com/slack_timesmanager/diary/DiaryDynamoRepository.java
@@ -1,5 +1,12 @@
 package com.slack_timesmanager.diary;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class DiaryDynamoRepository {
 
+	public DiaryDynamoRepository() {
+		super();
+	}
+	
 }
