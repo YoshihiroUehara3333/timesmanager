95a128523430b47ff23ff6a5f37d87f4038df847
YoshihiroUehara_desktop <relect.jpn@gmail.com>
2026-01-06 20:31:58 +0900

    fix/ ルーティングの定義ミス修正

--- slackbot/src/shared/constants/BackendRouting.js
+++ slackbot/src/shared/constants/BackendRouting.js
@@ -16,10 +16,10 @@ exports.BackendRouting = Object.freeze({
   },
   DAILYREPORT: {
     GETBYDATE: (date) => ({ method: 'GET', path: `${API_BASE}/dailyreport/${date}` }),
-    SAVE: (date) => ({ method: 'PUT', path: `${API_BASE}/dailyreport${date}` }),
+    SAVE: (date) => ({ method: 'PUT', path: `${API_BASE}/dailyreport/${date}` }),
   },
   ATTENDANCE: {
     SAVE: (date) => ({ method: 'PUT', path: `${API_BASE}/attendance/${date}` }),
-    GETBYDATE: (date) => ({ method: 'GET', path: `${API_BASE}/attendance${date}` }),
+    GETBYDATE: (date) => ({ method: 'GET', path: `${API_BASE}/attendance/${date}` }),
   }
 })
